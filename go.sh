ac8972c106360f5bd5fcc4b29fd778ca59d4de41
hairongsi <1947290962@qq.com>
2016-04-12 16:54:07 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "1947290962@qq.com"
+git config --global user.name "hairongsi"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push
+start http://hairongsi.github.io
